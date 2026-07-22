6c8323ce2149eef108ee446e1aa787a83ec7fc93
Johanna Rhey S. Alcarez <alcarezjohannahrhey@gmail.com>
2026-07-23 02:01:24 +0800

    Collect static files for Render

--- SchoolScholarshipSystem/settings.py
+++ SchoolScholarshipSystem/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'SchoolScholarshipSystem.urls'
@@ -125,4 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
